1226120909cd23fea6254f3733d70e3204ffd063
Simon Yakunin <simon@blockvis.com>
2018-04-23 11:22:20 +0300

    Current implementation

--- contracts/zeppelin/PriorityQueue.sol
+++ contracts/zeppelin/PriorityQueue.sol
@@ -6,105 +6,126 @@ contract PriorityQueue {
     uint256[] public items;
     mapping(uint256 => uint256) public dataStore;
 
+    event Log(string message, uint256 logData);
+
+    function PriorityQueue() public {
+        items.push(0);
+    }
+
+    function log(string message, uint256 logData) internal {
+        emit Log(message, logData);
+    }
+
     function push(uint256 key, uint256 data) public {
-        items.push(key);
         dataStore[key] = data;
-        bubbleUp(items.length - 1);
+        assembly {
+
+            sstore(items_slot, add(sload(items_slot), 1)) // items.length++
+
+            let currentIdx := sub(sload(items_slot), 1) // currentIdx = items.length - 1
+            if eq(currentIdx, 0) { return (0, 0) } // if (currentIdx == 0) return;
+
+            mstore(0x0, items_slot)
+            let arrStart := keccak256(0, 32)
+            let parentIdx := currentIdx
+
+            for {} gt(currentIdx, 0) {} { // while (currentIdx > 0)
+                parentIdx := div(parentIdx, 2) // parentIdx = parentIdx / 2
+                let parent := sload(add(arrStart, parentIdx)) // parent = items[parentIdx]
+                switch gt(parent, key) // if parent > key
+                case 1 {
+                    sstore(add(arrStart, currentIdx), parent) // items[currentIdx] = parent
+                    currentIdx := parentIdx
+                }
+                default { // else
+                    sstore(add(arrStart, currentIdx), key) // items[currentIdx] = key
+                    return (0, 0) // break
+                }
+            }
+            sstore(add(arrStart, currentIdx), key) // items[currentIdx] = current
+        }
     }
 
     function takeMin() public returns (uint256) {
-        if (items.length == 0) return 0;
-        uint256 data = dataStore[items[0]];
-        uint256 last = items.length - 1;
-        items[0] = items[last];
-        dataStore[last] = 0;
-        items[last] = 0;
-        items.length--;
-        bubbleDown();
+        if (items.length < 2) return 0;
+
+        uint256 arrStart;
+        uint256 key;
+        assembly {
+            mstore(0x0, items_slot)
+            arrStart := keccak256(0, 32)
+            key := sload(add(arrStart, 1)) // key = items[1]
+        }
+        uint256 data = dataStore[key];
+        dataStore[key] = 0;
+
+        uint256 last;
+
+        assembly {
+            let lastIndex := sub(sload(items_slot), 1) // lastIndex = items.length - 1
+            last := sload(add(arrStart, lastIndex))
+            //sstore(add(arrStart, 1), ) // items[1] = items[lastIndex]
+            sstore(add(arrStart, lastIndex), 0) // items[lastIndex] = 0
+            sstore(items_slot, lastIndex) // items.length = last
+        }
+        bubbleDown(last);
         return data;
     }
 
-    function size() public view returns (uint256) {
-        return items.length;
-    }
-
-    function bubbleUp(uint256 index) internal {
-        if (index == 0) return;
-
-        uint256 current = items[index];
-        uint256 parent = 0;
-        uint256 parentIndex = 0;
-
-        uint256 arrStart;
-        assembly {
-            arrStart := keccak256(items_slot, 32)
-        }
-        while (index > 0) {
-
-            assembly {
-                parentIndex := div(sub(index, 1), 2)
-                parent := sload(add(arrStart, parentIndex))
-            }
-
-            if (current >= parent) break;
-
-            assembly {
-                sstore(add(arrStart, parentIndex), current)
-                sstore(add(arrStart, index), parent)
-
-            }
-
-            index = parentIndex;
-            current = parent;
-        }
-    }
-
-    function bubbleDown() internal {
+    function bubbleDown(uint256 current) internal {
         uint256 itemCount = items.length;
-        if (itemCount == 0) return;
-        uint256 smallest = 0;
-        uint256 current;
-        uint256 leftIndex;
-        uint256 rightIndex;
-        uint256 leftValue;
-        uint256 rightValue;
         uint256 arrStart;
-        uint256 smallestValue = items[smallest];
-        uint256 currentValue;
         assembly {
-            arrStart := keccak256(items_slot, 32)
+            mstore(0x0, items_slot)
+            arrStart := keccak256(0, 32)
         }
+        uint256 currentIdx = 1;
+        uint256 smallestIdx = 1;
+        uint256 smallest = current;
+        uint256 leftChildIdx;
+        uint256 rightChildIdx;
+        uint256 left;
+        uint256 right;
 
         while (true) {
-            current = smallest;
-            currentValue = smallestValue;
-            leftIndex = 2 * smallest + 1;
-            rightIndex = 2 * smallest + 2;
-            if (leftIndex < itemCount) {
+            leftChildIdx = 2 * currentIdx;
+            rightChildIdx = leftChildIdx + 1;
+            if (leftChildIdx < itemCount) {
                 assembly {
-                    leftValue := sload(add(arrStart, leftIndex))
+                    left := sload(add(arrStart, leftChildIdx)) // left = items[leftChildIdx]
                 }
-
-                if (leftValue < smallestValue) {
-                    smallest = leftIndex;
-                    smallestValue = leftValue;
-                }
-            } else if (rightIndex < itemCount) {
-                assembly {
-                    rightValue := sload(add(arrStart, rightIndex))
-                }
-                if (rightValue < smallestValue) {
-                    smallest = rightIndex;
-                    smallestValue = rightValue;
+                if (smallest > left) {
+                    smallestIdx = leftChildIdx;
+                    smallest = left;
                 }
             }
-
-            if (smallest == current) break;
+            if (rightChildIdx < itemCount) {
+                assembly {
+                    right := sload(add(arrStart, rightChildIdx)) // left = items[rightChildIdx]
+                }
+                if (smallest > right) {
+                    smallestIdx = rightChildIdx;
+                    smallest = right;
+                }
+            }
+            if (smallestIdx == currentIdx) break;
 
             assembly {
-                sstore(add(arrStart, smallest), currentValue)
-                sstore(add(arrStart, current), smallestValue)
+                sstore(add(arrStart, currentIdx), smallest) // items[currentIdx] = smallest
             }
+            currentIdx = smallestIdx;
         }
+        assembly {
+            sstore(add(arrStart, smallestIdx), current) // items[smallestIdx] = current
+        }
+    }
+
+    function min() public view returns (uint256) {
+        if (items.length == 0) return 0;
+        return dataStore[items[1]];
+    }
+
+    function size() public view returns (uint256) {
+        return items.length - 1;
     }
 }
--- test/priorityqueue.js
+++ test/priorityqueue.js
@@ -20,37 +20,93 @@ contract.only('PriorityQueue', async (accounts) => {
 
         expect(await contract.size()).to.be.bignumber.equal(1);
 
-        expect(await contract.takeMin.call()).to.be.bignumber.equal(100);
+        expect(await contract.min()).to.be.bignumber.equal(100);
+        await contract.takeMin();
+    });
+
+    it('should store again data item', async () => {
+        await contract.push(1, 100);
+
+        expect(await contract.size()).to.be.bignumber.equal(1);
+
+        expect(await contract.min()).to.be.bignumber.equal(100);
+        await contract.takeMin();
+    });
+
+    it('should store data item', async () => {
+        await contract.push(2, 50);
+        await contract.push(1, 100);
+
+        expect(await contract.size()).to.be.bignumber.equal(2);
+
+        expect(await contract.min()).to.be.bignumber.equal(100);
+        await contract.takeMin();
+        expect(await contract.min()).to.be.bignumber.equal(50);
+        await contract.takeMin();
+    });
+
+    it('should store 4 data items', async () => {
+        await contract.push(4, 50);
+        await contract.push(3, 75);
+        await contract.push(2, 100);
+        await contract.push(1, 125);
+
+        expect(await contract.size()).to.be.bignumber.equal(4);
+
+        expect(await contract.min()).to.be.bignumber.equal(125);
+        await contract.takeMin();
+        expect(await contract.min()).to.be.bignumber.equal(100);
+        await contract.takeMin();
+        expect(await contract.min()).to.be.bignumber.equal(75);
+        await contract.takeMin();
+        expect(await contract.min()).to.be.bignumber.equal(50);
         await contract.takeMin();
     });
 
 
-    it('should store additional 3 data items', async () => {
-        await contract.push(4, 25);
-        await contract.push(3, 50);
-        await contract.push(2, 75);
+    it('should store 5 data items', async () => {
+        await contract.push(5, 25);
+        await contract.push(4, 50);
+        await contract.push(3, 75);
+        await contract.push(2, 100);
+        await contract.push(1, 125);
 
-        expect(await contract.size()).to.be.bignumber.equal(3);
+        expect(await contract.size()).to.be.bignumber.equal(5);
 
-        expect(await contract.takeMin.call()).to.be.bignumber.equal(75);
+        expect(await contract.min()).to.be.bignumber.equal(125);
+        await contract.takeMin();
+        expect(await contract.min()).to.be.bignumber.equal(100);
+        await contract.takeMin();
+        expect(await contract.min()).to.be.bignumber.equal(75);
+        await contract.takeMin();
+        expect(await contract.min()).to.be.bignumber.equal(50);
+        await contract.takeMin();
+        expect(await contract.min()).to.be.bignumber.equal(25);
         await contract.takeMin();
     });
 
-    it('should store additional many items', async () => {
+    it('should store many items', async () => {
         let sum = 0;
         let i;
-        for (i = 0; i <= 1000; i++) {
+        let count = 51;
+        for (i = 0; i < count; i++) {
             const tx = await contract.push(10000 - i, i * 3);
             sum += tx.receipt.gasUsed;
         }
+        expect(await contract.min()).to.be.bignumber.equal((i - 1) * 3);
+        const store = sum;
+        console.log('Storage gas:', store);
+        console.log('Average storage:', (store / count));
 
-        expect(await contract.takeMin.call()).to.be.bignumber.equal(50);
-        const tx1 = await contract.takeMin();
-        expect(await contract.takeMin.call()).to.be.bignumber.equal(25);
-        const tx2 = await contract.takeMin();
-        expect(await contract.takeMin.call()).to.be.bignumber.equal((i - 1) * 3);
+        const n = await contract.size();
+        for (i = 0; i < n; i++) {
+            const tx = await contract.takeMin();
+            sum += tx.receipt.gasUsed;
+        }
+        expect(await contract.size()).to.be.bignumber.equal(0);
 
-        sum += tx1.receipt.gasUsed + tx2.receipt.gasUsed;
+        console.log('Remove:' , (sum - store));
+        console.log('Average remove:', (sum - store) / count);
         console.log('Total gas:', sum);
     });
 
